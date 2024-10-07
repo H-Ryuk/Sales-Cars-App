e7a08ccff59f394b7d1c7f5146bb7ab85a5a1e17
H-Ryuk <hassan01saadaoui@gmail.com>
2024-10-07 17:36:56 +0100

    Add Global Exception Handler

---
+++ src/main/java/com/hassan/Advice/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.hassan.Advice;
+
+
+import com.hassan.Exception.TargetNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException ex){
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            errorMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+        return errorMap;
+    }
+
+
+    @ExceptionHandler(TargetNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(TargetNotFoundException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+
+}
